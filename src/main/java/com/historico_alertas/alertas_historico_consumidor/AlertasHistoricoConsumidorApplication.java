95df101271107513cbb70199951b0c75c9c04a02
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-03 03:21:40 -0300

    Creación de proyecto Java Spring Boot con integración a RabbitMW

---
+++ src/main/java/com/historico_alertas/alertas_historico_consumidor/AlertasHistoricoConsumidorApplication.java
@@ -0,0 +1,13 @@
+package com.historico_alertas.alertas_historico_consumidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertasHistoricoConsumidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertasHistoricoConsumidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/historico_alertas/alertas_historico_consumidor/AlertasHistoricoConsumidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historico_alertas.alertas_historico_consumidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertasHistoricoConsumidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
